98b1d351ea52f24f05e58f9e952e4ca20d5445e0
unknown <Jeconiah>
2026-01-29 13:10:05 +0530

    Add longest repeating character replacement

---
+++ sliding window problems/longest-repeating-character-replacement.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let count = {};
+    let left = 0;
+    let maxFreq = 0;
+    let maxLen = 0;
+
+    for(let right = 0; right<= s.length ; right++){
+         let ch = s[right];
+
+         count[ch] = (count[ch] || 0) + 1;
+
+         maxFreq = Math.max(maxFreq, count[ch]);
+
+         while((right - left + 1) - maxFreq > k){
+            count[s[left]]--;
+            left++;
+         }
+         maxLen = Math.max(maxLen, right - left + 1);
+    }
+    return maxLen;
+
+};
